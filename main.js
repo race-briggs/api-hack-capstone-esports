cfd9d53a0ed6d1dd12ce56f73cbb8d484aef80c1
Race Briggs <race.briggs@gmail.com>
2018-10-05 15:45:10 -0500

    improved css and html formatting, added watchSubmit function

--- main.js
+++ main.js
@@ -1,19 +1,47 @@
 'use strict';
 
-function formatSearchQuery {
+const pandaScoreApiKey = "oiTb1aNeODTo9jIMKINBpZ5zmGF50dNfZ3WQspL_-Xn6rBwuxs8";
+const twitchApiKey = "tszgw747t4ad80nlczn9t658cma89p";
+
+
+function formatSearchQuery(params) {
 
 }
 
-function displayResults {
+function displayResults(response, maxResults) {
 
 }
 
-function fetchData {
+function fetchTwitchStreamData(query, maxResults) {
 
 }
 
-function watchSubmit {
+function fetchTwitchClipsData(query, maxResults) {
 
+}
+
+function fetchEsportsData(query, maxResults) {
+
+}
+
+
+function watchSubmit() {
+	let searchTerm = $('.js-search').val();
+	let maxResults = $('.js-max-results').val();
+	$('#js-form').submit(event => {
+		event.preventDefault();
+		if($('.js-checkbox-1').checked){
+			fetchTwitchStreamData(searchTerm, maxResults);
+		};
+		if($('.js-checkbox-2').checked){
+			fetchTwitchClipsData(searchTerm, maxResults);
+		};
+		if($('.js-checkbox-3').checked){
+			fetchEsportsData(searchTerm, maxResults);
+		};
+		});
+	$('.js-search').val('');
+	$('.js-max-results').val('');
 }
 
 $(watchSubmit);
