be622d779fae3c48e7165197b43143acb2d1f586
Race Briggs <race.briggs@gmail.com>
2018-10-08 14:40:07 -0500

    added to fetch functions, applied css and html changes to see formatting

--- main.js
+++ main.js
@@ -4,19 +4,69 @@ const pandaScoreApiKey = "oiTb1aNeODTo9jIMKINBpZ5zmGF50dNfZ3WQspL_-Xn6rBwuxs8";
 const twitchApiKey = "tszgw747t4ad80nlczn9t658cma89p";
 
 
-function formatSearchQuery(params) {
-
-}
+/* function formatSearchQuery(params) {
+	console.log('formatting ran!');
+	const queryTerm = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+	return queryTerm.join('&');
+} */
 
 function displayResults(response, maxResults) {
-
+	console.log('displayResults ran!');
+	console.log(responseJson);
 }
 
-function fetchTwitchStreamData(query, maxResults) {
+function fetchTwitchStreamData(query, maxResults=10) {
 
+	const options = {
+		mode: 'cors',
+		method: 'GET',
+		headers: new Headers ({
+			'accept': 'application/json',
+			'Client-ID': twitchApiKey,
+		})
+	}
+
+	const url = 'https://api.twitch.tv/kraken/search/streams?query=' + encodeURIComponent(query)
+
+	fetch(url, options)
+		.then(response => {
+			if(response.ok){
+				return response.json();
+			} else {
+				throw new Error(response.statusText);
+			}
+		})
+		.then(responseJson => displayResults(responseJson, maxResults))
+		.catch(e => {
+			alert('An error occurred.');
+		});
 }
 
 function fetchTwitchClipsData(query, maxResults) {
+
+	const options = {
+		mode: 'cors',
+		method: 'GET',
+		headers: new Headers ({
+			'accept': 'application/json',
+			'Client-ID': twitchApiKey,
+		})
+	};
+
+	const url = 'https://api.twitch.tv/kraken/clips/top?game=' + formatSearchQuery(query)
+
+	fetch(url, options)
+		.then(response => {
+			if(response.ok){
+				return response.json();
+			} else {
+				throw new Error(response.statusText);
+			}
+		})
+		.then(responseJson => displayResults(responseJson, maxResults))
+		.catch(e => {
+			alert('An error occurred.');
+		});
 
 }
 
@@ -26,17 +76,17 @@ function fetchEsportsData(query, maxResults) {
 
 
 function watchSubmit() {
-	let searchTerm = $('.js-search').val();
-	let maxResults = $('.js-max-results').val();
 	$('#js-form').submit(event => {
 		event.preventDefault();
-		if($('.js-checkbox-1').checked){
+		let searchTerm = $('.js-search').val();
+		let maxResults = $('.js-max-results').val();
+		if($('.js-checkbox-1').is(":checked")){
 			fetchTwitchStreamData(searchTerm, maxResults);
 		};
-		if($('.js-checkbox-2').checked){
+		if($('.js-checkbox-2').is(":checked")){
 			fetchTwitchClipsData(searchTerm, maxResults);
 		};
-		if($('.js-checkbox-3').checked){
+		if($('.js-checkbox-3').is(":checked")){
 			fetchEsportsData(searchTerm, maxResults);
 		};
 		});
