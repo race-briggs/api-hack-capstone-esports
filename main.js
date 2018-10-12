6392a6cfa881871d833b1af22cfffb2ab2172fa2
Race Briggs <race.briggs@gmail.com>
2018-10-12 12:12:26 -0500

    pushing up for more peer editing, testing functionality, also altered css

--- main.js
+++ main.js
@@ -10,65 +10,56 @@ const twitchApiKey = "tszgw747t4ad80nlczn9t658cma89p";
 	return queryTerm.join('&');
 } */
 
-let streamCalled = 0;
-let clipsCalled = 0;
-let esportsCalled = 0;
 let maxResults = 10;
 
-function displayResults(responseJson, maxResults) {
-	console.log('displayResults ran!');
+function displayStreams(responseJson, maxResults) {
+	console.log('displayStreams ran!');
 	console.log(responseJson);
-	if(streamCalled > 0){
-			$('.js-results-1').empty();
-			$('.js-results-1').append(
-				`<h3>Top Live Streams</h3>
-				<ul class="stream-results-list"></ul>`
-				);
-			for(let i = 0; i < maxResults; i++) {
-				$('.stream-results-list').append(`
-					<li class="stream-result">
-						<h4>${responseJson.streams[i].channel.name}</h4>
-						<img class="stream-preview" src="${responseJson.streams[i].preview.medium}">
-						<a class="stream-url" href="${responseJson.streams[i].channel.url}">${responseJson.streams[i].channel.url}</a>
-					</li>
-					`)
-			};
-			streamsCalled = 0;
-		};
-	if(clipsCalled > 0){
-			$('.js-results-2').empty();
-			$('.js-results-2').append(
-				`<h3>Top Clips</h3>
-				<ul class="clip-results-list"></ul>`
-				);
-			for(let i = 0; i < maxResults; i++) {
-				$('.clip-results-list').append(`
-					<li class="clip-li">
-						<h4>${responseJson.clips[i].title}</h4>
-						<iframe
-				    src="${responseJson.clips[i].embed_url}"
-				    height="300"
-				    width="400"
-				    preload="metadata"
-				    frameborder=""
-				    scrolling="no"
-				    allowfullscreen="true"
-				    autoplay="false">
-						</iframe>
-					</li>
-					`)
-			};
-			clipsCalled = 0;
-		};
+	$('.js-results-1').empty();
+	$('.js-results-1').append(
+		`<h3>Top Live Streams</h3>
+		<ul class="stream-results-list"></ul>`
+		);
+	for(let i = 0; i < maxResults; i++) {
+		$('.stream-results-list').append(`
+			<li class="stream-result">
+				<h4>${responseJson.streams[i].channel.name}</h4>
+				<img class="stream-preview" src="${responseJson.streams[i].preview.medium}">
+				<a class="stream-url" href="${responseJson.streams[i].channel.url}">${responseJson.streams[i].channel.url}</a>
+			</li>
+			`)
+	};
+};
 
-	
-	
-	esportsCalled = 0;
-}
+
+function displayClips(responseJson, maxResults) {
+	console.log('displayClips ran!');
+	console.log(responseJson);
+	$('.js-results-2').empty();
+	$('.js-results-2').append(
+		`<h3>Top Clips</h3>
+		<ul class="clip-results-list"></ul>`
+		);
+	for(let i = 0; i < maxResults; i++) {
+		$('.clip-results-list').append(`
+			<li class="clip-li">
+				<h4>${responseJson.clips[i].title}</h4>
+				<iframe
+		    src="https://clips.twitch.tv/embed?clip=${responseJson.clips[i].slug}"
+		    height="300"
+		    width="400"
+		    preload="metadata"
+		    frameborder=""
+		    scrolling="no"
+		    allowfullscreen="true"
+		    autoplay="false">
+				</iframe>
+			</li>
+			`)
+	};
+};
 
 function fetchTwitchStreamData(query, maxResults) {
-
-	streamCalled++;
 
 	const options = {
 		method: 'GET',
@@ -88,15 +79,13 @@ function fetchTwitchStreamData(query, maxResults) {
 				throw new Error(response.statusText);
 			};
 		})
-		.then(responseJson => displayResults(responseJson, maxResults))
+		.then(responseJson => displayStreams(responseJson, maxResults))
 		.catch(e => {
 			console.log(e);
 		});
 }
 
 function fetchTwitchClipsData(query, maxResults) {
-
-	clipsCalled++;
 
 	const options = {
 		method: 'GET',
@@ -117,11 +106,10 @@ function fetchTwitchClipsData(query, maxResults) {
 				throw new Error(response);
 			};
 		})
-		.then(responseJson => displayResults(responseJson, maxResults))
+		.then(responseJson => displayClips(responseJson, maxResults))
 		.catch(e => {
 			console.log(e);
 		});
-
 }
 
 function fetchEsportsData(query, maxResults) {
